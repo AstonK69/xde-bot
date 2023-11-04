0fb43c3b8d58c05bc45a6e56ea505c5e05283ff6
Aston <69041668+AstonK69@users.noreply.github.com>
2023-11-04 23:29:03 +0000

    moved embeds to seperate file and added gt4 embed

---
+++ src/data/league_embeds.py
@@ -0,0 +1,167 @@
+import discord
+from src.load import Colours
+
+f1_05_embed = discord.Embed(title="XDE Formula 1 2005 League",
+                            description="[Live Timings](http://ac.xde.nl:8772/live-timing?server=1) | [Server Link](https://acstuff.ru/s/q:race/online/join?httpPort=9604&ip=89.117.56.93) | [Website](https://xde.nl/)",
+                            colour=Colours.standard)
+f1_05_embed.set_image(
+    url="https://cdn.discordapp.com/attachments/1140717990350966879/1168250192722661547/Screenshot_xde_vrc_2005_mclaren_mp420_acu_hungaroring_16-9-123-18-3-48.png?ex=655114c6&is=653e9fc6&hm=e4a97957555232f853243a914f2d2d0c2c1ed16b616092502ddfe53b588b4c00&")
+
+f1_05_embed.add_field(name="", value="""**```ml
+‎
+    XTREME DUTCH ELITE FORMULA ONE 2005 LEAGUE
+        ```**""", inline=False)
+
+f1_05_embed.add_field(name="",
+                      value="The current league we are running in XDE is the F1 2005 cars, there is 8 rounds of different style tracks which makes for some amazing racing. They run on sunday evenings for europe and below we have made timestamps to put all session times into your own timezone. All the necessary information about the league and signing up is below:",
+                      inline=False)
+
+f1_05_embed.add_field(name="Calendar", value="here are all the different tracks on the calendar for the league:",
+                      inline=False)
+f1_05_embed.add_field(name="", value=""">>> **Round 1**
+Interlagos
+Laps: 20""")
+f1_05_embed.add_field(name="", value=""">>> **Round 2**
+Red Bull Ring
+Laps: 22""")
+f1_05_embed.add_field(name="", value=""">>> **Round 3**
+Maggiore
+Laps: 16""")
+f1_05_embed.add_field(name="", value=""">>> **Round 4**
+Silverstone
+Laps: 18""")
+f1_05_embed.add_field(name="", value=""">>> **Round 5**
+Hungaroring
+Laps: 17""")
+f1_05_embed.add_field(name="", value=""">>> **Round 6**
+Istanbul Park
+Laps: 16""")
+f1_05_embed.add_field(name="", value=""">>> **Round 7**
+Hockenheimring
+Laps: 19""")
+f1_05_embed.add_field(name="", value=""">>> **Round 8**
+Spa-Francorchamps
+Laps: 13""")
+
+f1_05_embed.add_field(name="Session Times", value="""The layout of the evening consists of a practice session which we dont require you to be there for, our briefing which we expect everyone to be in voice chat for, qualifying and the 2 races. The second race starting grid is the reverse of the finishing results of race one. Below is the times and length for all the sessions on the day:
+(Times are subject to change and may vary from weekend to weekend)""",
+                      inline=False)
+f1_05_embed.add_field(name="", value=""">>> **Practice**
+<t:1698950700:t> - <t:1698952500:t> (30 Minutes)""", inline=False)
+f1_05_embed.add_field(name="", value=""">>> **Briefing**
+<t:1698952500:t> - <t:1698953400:t> (15 Minutes)""", inline=False)
+f1_05_embed.add_field(name="", value=""">>> **Qualifying**
+<t:1698953400:t> - <t:1698954600:t> (20 Minutes)""", inline=False)
+f1_05_embed.add_field(name="", value=""">>> **Race 1 - Standard**
+<t:1698954600:t> - <t:1698956400:t> (30 Minutes)""", inline=False)
+f1_05_embed.add_field(name="", value=""">>> **Race 2 - Reverse Grid**
+<t:1698956400:t> - <t:1698958200:t> (30 Minutes)""", inline=False)
+
+f1_05_embed.add_field(name="Discord channels",
+                      value="Here are all the channel links for all the channels dedicated to the league:", inline=False)
+f1_05_embed.add_field(name="", value="""> Updates => <#1142909987014852809>
+> Points => <#1142910050722127923>
+> Rules => <#1142910537961853061>
+> Sign-up => <#1142910131198234816>
+> General chat => <#1142910189801046076>
+> Liveries => <#1142910230439673978>""", inline=False)
+
+f1_05_embed.add_field(name="Useful links",
+                      value="Below are all the useful links that you will need if you're taking part in the formula 1 league:",
+                      inline=False)
+f1_05_embed.add_field(name="", value=""">>> **Rulebook** (We do encourage you to look briefly at it)
+https://docs.google.com/document/d/1p1DRlkKzuKrGw3CeRMiDGooGnSOgxnrB8AGG6T5AGmg/edit?usp=sharing
+
+**Download links** (Cars, Tracks, Safety car and Liveries)
+https://docs.google.com/document/d/1p1DRlkKzuKrGw3CeRMiDGooGnSOgxnrB8AGG6T5AGmg/edit?usp=sharing
+
+**Live Timing Page**
+http://xde.nl:8772/live-timing?server=1
+
+**Server Link**
+https://acstuff.ru/s/q:race/online/join?httpPort=9604&ip=89.117.56.93""")
+
+f1_05_embed.set_footer(text=f"Xtreme Dutch Elite ・ 2023 | Created by Aston",
+                       icon_url='https://cdn.discordapp.com/attachments/940889123437309972/1168232344256258058/smaller_xde_logo.png?ex=65510427&is=653e8f27&hm=5f07726900ba157438dc6da3be2bcd10db6e5e3daa9825e4814dd75ff0fa677d&')
+
+########################################################################################################################
+##############################################   Porsche GT4 Challenge   ###############################################
+########################################################################################################################
+
+gt4_embed = discord.Embed(title="XDE Porsche GT4 Challenge",
+                            description="[Live Timings](http://ac.xde.nl:8772/live-timing?server=1) | [Server Link](https://acstuff.ru/s/q:race/online/join?httpPort=9604&ip=89.117.56.93) | [Website](https://xde.nl/)",
+                            colour=Colours.standard)
+
+gt4_embed.set_image(
+    url="https://cdn.discordapp.com/attachments/777524646420807711/1169369988382081144/Screenshot_ks_porsche_cayman_gt4_clubsport_ks_barcelona_1-10-123-21-16-55.png?ex=655527ab&is=6542b2ab&hm=2e1ddc0ee7af6dd915eda8ebb5f450ca6a57aeef2c1305fb6264ac42836c6197&")
+
+gt4_embed.add_field(name="", value="""**```ml
+‎
+     XTREME DUTCH ELITE PORSCHE GT4 CHALLENGE
+        ```**""", inline=False)
+
+gt4_embed.add_field(name="",
+                      value="The current league we are running in XDE is in the Porsche Cayman GT4 Clubsport cars, there is 6 rounds of different style tracks which makes for some amazing racing. We run our races on sunday evenings for europe and below we have made timestamps to put all session times into your own timezone. All the necessary information about the league and signing up is below:",
+                      inline=False)
+
+gt4_embed.add_field(name="Calendar", value="here are all the different tracks on the calendar for the league:",
+                      inline=False)
+gt4_embed.add_field(name="", value=""">>> **Round 1**
+Barcelona
+Laps: `TBA`""")
+gt4_embed.add_field(name="", value=""">>> **Round 2**
+Palmadera
+Laps: `TBA`""")
+gt4_embed.add_field(name="", value=""">>> **Round 3**
+Brands Hatch
+Laps: `TBA`""")
+gt4_embed.add_field(name="", value=""">>> **Round 4**
+Sebring
+Laps: `TBA`""")
+gt4_embed.add_field(name="", value=""">>> **Round 5**
+Monza
+Laps: `TBA`""")
+gt4_embed.add_field(name="", value=""">>> **Round 6**
+Deep Forest
+Laps: `TBA`""")
+
+gt4_embed.add_field(name="Session Times", value="""The layout of the evening consists of a practice session which we dont require you to be there for, our briefing which we expect everyone to be in voice chat for, qualifying and the 2 races. The second race starting grid is the reverse of the finishing results of race one. Below is the times and length for all the sessions on the day:
+(Times are subject to change and may vary from weekend to weekend)""",
+                      inline=False)
+gt4_embed.add_field(name="", value=""">>> **Practice**
+<t:1698950700:t> - <t:1698952500:t> (30 Minutes)""", inline=False)
+gt4_embed.add_field(name="", value=""">>> **Briefing**
+<t:1698952500:t> - <t:1698953400:t> (15 Minutes)""", inline=False)
+gt4_embed.add_field(name="", value=""">>> **Qualifying**
+<t:1698953400:t> - <t:1698954600:t> (20 Minutes)""", inline=False)
+gt4_embed.add_field(name="", value=""">>> **Race 1 - Standard**
+<t:1698954600:t> - <t:1698956400:t> (30 Minutes)""", inline=False)
+gt4_embed.add_field(name="", value=""">>> **Race 2 - Reverse Grid**
+<t:1698956400:t> - <t:1698958200:t> (30 Minutes)""", inline=False)
+
+gt4_embed.add_field(name="Discord channels",
+                      value="Here are all the channel links for all the channels dedicated to the league:", inline=False)
+gt4_embed.add_field(name="", value="""> Updates => <#1169964649467346954>
+> Points => <#1169964794963562546>
+> Rules => <#1169965000941633576>
+> Sign-up => <#1169965237932413021>
+> General chat => <#1169965314503622737>
+> Liveries => <#1169965404383354910>""", inline=False)
+
+gt4_embed.add_field(name="Useful links",
+                      value="Below are all the useful links that you will need if you're taking part in the formula 1 league:",
+                      inline=False)
+gt4_embed.add_field(name="", value=""">>> **Rulebook** (We do encourage you to look briefly at it)
+https://docs.google.com/document/d/1W16CzF-UO7cnQPsua9SBRIkltdQPvZgaURMEEec6818/edit?usp=drive_link
+
+**Download links** (Cars, Tracks, Safety car and Liveries)
+https://drive.google.com/drive/folders/1_eNTXLmLcNIt4Wdrm1XYLs6zYvKad4Vv?usp=sharing
+
+**Live Timing Page**
+http://xde.nl:8772/live-timing?server=1
+
+**Server Link**
+https://acstuff.ru/s/q:race/online/join?httpPort=9604&ip=89.117.56.93""")
+
+gt4_embed.set_footer(text=f"Xtreme Dutch Elite ・ 2023 | Created by Aston",
+                       icon_url='https://cdn.discordapp.com/attachments/940889123437309972/1168232344256258058/smaller_xde_logo.png?ex=65510427&is=653e8f27&hm=5f07726900ba157438dc6da3be2bcd10db6e5e3daa9825e4814dd75ff0fa677d&')
